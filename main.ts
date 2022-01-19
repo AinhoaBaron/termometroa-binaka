c1a0d2407790bd05045fc353e10f2c0e624a5274
AinhoaBaron <93262452+AinhoaBaron@users.noreply.github.com>
2022-01-19 14:05:45 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,8 @@
+let TenpExt = 0
+radio.setGroup(4)
+basic.forever(function () {
+    TenpExt = input.temperature()
+    TenpExt = TenpExt - 2
+    basic.showString("" + (TenpExt))
+    radio.sendNumber(TenpExt)
+})
